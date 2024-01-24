6b1dd73c16bce39a590a430b0aadd6fbff21f995
Gabriel <bristotgl@gmail.com>
2024-01-24 19:49:51 -0300

    first commit

---
+++ src/test/java/com/example/gymplanner/GymPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gymplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
